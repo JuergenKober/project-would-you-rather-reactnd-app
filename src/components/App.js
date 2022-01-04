8b4c79168e1c9c57adcf805724f93ac9b65904c5
mindeleven <mindeleven@posteo.de>
2022-01-04 11:35:41 +0100

    Modifying starter code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-       APP component
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Starter Code
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
