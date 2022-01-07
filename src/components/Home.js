0c8601fcc1c83c78044da2385a36bf59c4b1ca4f
mindeleven <mindeleven@posteo.de>
2022-01-07 14:55:44 +0100

    Splitting the question list in answered and unanswered questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,18 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h3 className='center'>Would you rather... ?</h3>
+        <h1 className='center'>Would you rather... ?</h1>
+        <h3 className='center'>Answered Questions</h3>
         <ul className='dashboard-list'>
-        {this.props.questionIds.map((id) => (
+        {this.props.answeredQuestionIds.map((id) => (
+          <li key={id}>
+            <div>QUESTION ID: {id}</div>
+          </li>
+        ))}
+        </ul>
+        <h3 className='center'>Unanswered Questions</h3>
+        <ul className='dashboard-list'>
+        {this.props.unansweredQuestionIds.map((id) => (
           <li key={id}>
             <div>QUESTION ID: {id}</div>
           </li>
@@ -18,11 +27,20 @@ class Home extends Component {
   }
 };
 
-function mapStateToProps ({ questions }) {
+function mapStateToProps ({ questions, authedUser, users }) {
+
+  const answeredQuestionIds = Object.keys(questions)
+    .filter((id) => users[authedUser].answers.hasOwnProperty(id))
+     .sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
+  const unansweredQuestionIds = Object.keys(questions)
+  .filter((id) => !users[authedUser].answers.hasOwnProperty(id))
+   .sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
   return {
-    questionIds: Object.keys(questions)
-      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
-  }
+    answeredQuestionIds,
+    unansweredQuestionIds
+  };
 }
 
 export default connect(mapStateToProps)(Home)
