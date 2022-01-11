0112c84758597ef4241ecf973f9e3ceab38b752f
mindeleven <mindeleven@posteo.de>
2022-01-11 15:18:19 +0100

    Intergrating logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function Header(props) {
   const { user, dispatch } = props;
+
+  const logoutUser = () => {
+		console.log('logging out');
+	};
+
   return (
 		<Fragment>
       <Card>
         <h2>Header</h2>
-        <Card.Text>You're logged in as {user.name}</Card.Text>
+        <Card.Text>
+          You're logged in as {user.name}
+        </Card.Text>
+        <Button onClick={logoutUser}>
+          Logout
+        </Button>
       </Card>
     </Fragment>
   );
