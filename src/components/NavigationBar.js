650012d00f84417785a3fa9d08ee4eb21e5f23a3
mindeleven <mindeleven@posteo.de>
2022-01-13 11:08:14 +0100

    Moving logout functionality to the NavigationBar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,9 +3,16 @@ import { connect } from 'react-redux';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { unsetAuthedUser } from '../actions/authedUser';
 
 function NavigationBar(props) {
-	const { user, dispatch } = props;
+  const { user, dispatch } = props;
+
+  const logoutUser = () => {
+		console.log('logging out');
+    dispatch(unsetAuthedUser());
+	};
 
   return (
 		<Fragment>
@@ -15,8 +22,11 @@ function NavigationBar(props) {
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
+              Signed in as: <a href="#login">{user.name}</a>
             </Navbar.Text>
+            <Button onClick={logoutUser}>
+              Logout
+            </Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
