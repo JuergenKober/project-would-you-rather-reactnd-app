52d215ca1fb63fd2ebb817b9218e5d705e8faf22
mindeleven <mindeleven@posteo.de>
2022-01-14 15:53:30 +0100

    Prettifying the UI of the QuestionStats component

--- src/components/QuestionStats.js
+++ src/components/QuestionStats.js
@@ -5,6 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
+import Image from 'react-bootstrap/Image';
 
 class QuestionForm extends Component {
 
@@ -19,13 +20,22 @@ class QuestionForm extends Component {
     const alertBoxColorTwo = optionTwo.votes.includes(authedUser) ? 'success' : 'light';
 
     return (
-      <Row>
-        <Col>
-          <Card>
+      <Row className="justify-content-center">
+        <Col md={10} xs={12}>
+          <Card className="m-3">
             <Card.Header>
-              {author.name} asks, would you rather
+              <Image
+                src={author.avatarURL}
+                roundedCircle
+                fluid
+                width="35"
+                height="35"
+                className="mx-3"
+                alt={author.name}
+              />
+              <b>{author.name}</b> wants to know, would you rather...
             </Card.Header>
-            <Card.Body>
+            <Card.Body className="mx-3">
               <Alert variant={alertBoxColorOne}>
                 <Card.Text>
                   {optionOne.text}...?<br />
