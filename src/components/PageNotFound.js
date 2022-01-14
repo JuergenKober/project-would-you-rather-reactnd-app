e12ac73c095d4269a08fda39b5d6d6dd5319c2f3
mindeleven <mindeleven@posteo.de>
2022-01-14 15:38:15 +0100

    Prettifying the UI of the PageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 class PageNotFound extends Component {
   render () {
   	return (
-      <Row>
-        <Card>
-          <Card.Header>
-            Oh my, the dreaded 404 error
-          </Card.Header>
-          <Card.Body>
-            <Card.Text>
-              We're really sorry but we couldn't find this page
-            </Card.Text>
-            <Link to="/">
-              Go back to Home page
-            </Link>
-          </Card.Body>
-
-        </Card>
+      <Row className="justify-content-center">
+        <Col md={10} xs={12}>
+          <Card className="m-3">
+            <Card.Header>
+              Oh my, the dreaded 404 error
+            </Card.Header>
+            <Card.Body>
+              <Card.Text>
+                We're really sorry but we couldn't find this page
+              </Card.Text>
+              <Link to="/">
+                Go back to Home page
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
   	);
   }
