43da5fd241323336a6da52c539765fe54442bab5
mindeleven <mindeleven@posteo.de>
2022-01-14 13:26:22 +0100

    Some more elaborating on the UI and functionality of the leaderboard

--- src/components/LeaderBoardCard.js
+++ src/components/LeaderBoardCard.js
@@ -1,22 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class LeaderBoardCard extends Component {
   render() {
     const { user } = this.props;
 
 		return (
-			<Fragment>
-				<h2 className="text-center">
-					<small>LeaderBoardCard</small>
-				</h2>
-        <Card>
-          <Card.Text>
-            {user.name}
-          </Card.Text>
-        </Card>
-			</Fragment>
+			<Row className="justify-content-center">
+        <Col>
+          <Card>
+            <Card.Header>
+              {user.name}
+            </Card.Header>
+            <Card.Body className="justify-content-center">
+              <Card.Text>
+                ...has asked {user.questions.length} questions
+                <br />
+                ...has answered {Object.keys(user.answers).length} questions
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+			</Row>
 		);
 	};
 };
