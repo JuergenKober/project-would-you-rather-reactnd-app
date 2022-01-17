7466039b5b4c1a4f4eede5e5cafa1f9e03666a60
mindeleven <mindeleven@posteo.de>
2022-01-17 16:22:34 +0100

    Adding user images to the leaderboard

--- src/components/LeaderBoardCard.js
+++ src/components/LeaderBoardCard.js
@@ -3,19 +3,29 @@ import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 class LeaderBoardCard extends Component {
   render() {
     const { user } = this.props;
 
 		return (
-			<Row className="justify-content-center">
-        <Col>
-          <Card>
+      <Row className="justify-content-center">
+        <Col md={10} xs={12}>
+          <Card className="m-3">
             <Card.Header>
-              {user.name}
+              <Image
+                src={user.avatarURL}
+                roundedCircle
+                fluid
+                width="35"
+                height="35"
+                className="mx-3"
+                alt={user.name}
+              />
+              <b>{user.name}</b>
             </Card.Header>
-            <Card.Body className="justify-content-center">
+            <Card.Body className="mx-3">
               <Card.Text>
                 ...has asked {user.questions.length} questions
                 <br />
